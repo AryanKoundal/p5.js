40e002e7eb426ba3dd08c35c2ce1bbc5037b52e5
AryanKoundal <aryankoundal474@gmail.com>
2023-06-15 19:25:58 +0530

    added notes

--- src/webgl/shaders/webgl2Compatibility.glsl
+++ src/webgl/shaders/webgl2Compatibility.glsl
@@ -1,5 +1,4 @@
-// ? is the glsl not indented or im missing a formatter?
-// ? i can't understand the flow of this program
+// ? is the glsl not indented or im missing a formatter? : not formatted as a writing style for preprocessors
 
 // ifdef checks a macro, if the macro WEBGL2 is defined
 // do the following actions
@@ -12,26 +11,26 @@
     #ifdef FRAGMENT_SHADER
         // creates a vec4 and creates a macro with the name OUT_COLOR which will be replaced 
         // by out vec4 outColor
-        out vec4 outColor; // ? why is not a # in the begg of this line
+        out vec4 outColor; // ? why is not a # in the begg of this line: cause it would be part of a shader prog
         #define OUT_COLOR outColor
     #endif
 
     #define TEXTURE texture
+#else
+    // if there is a frag shader then replace all IN by varying
+    #ifdef FRAGMENT_SHADER
+        #define IN varying
+    // else it will be a vert shader then replace all IN by attributes
     #else
-        // if there is a frag shader then replace all varying by IN
-        #ifdef FRAGMENT_SHADER
-            #define IN varying
-        // else it will be a vert shader then replace all attributes by IN
-        #else
-            #define IN attribute
-        #endif
+        #define IN attribute
+    #endif
 
-    // replace all varying by OUT, but we have already confirmed above that it's not a frag shader
+    // replace all OUT  by varying, but we have already confirmed above that it's not a frag shader
     #define OUT varying
+    #define TEXTURE texture2D
 
     // ? why didn't we add the line below to the frag section above
-    #define TEXTURE texture2D
-    // if there is a frag shader replace gl_FragColor by OUT_COLOR
+    // if there is a frag shader replace OUT_COLOR by  gl_FragColor
     #ifdef FRAGMENT_SHADER
         #define OUT_COLOR gl_FragColor
     #endif
